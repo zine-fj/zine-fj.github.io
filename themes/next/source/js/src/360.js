4ec985a4d2eb338c849cb7f4a2fcb72a5d62561b
zine-fj <747810974@qq.com>
2018-06-14 10:58:35 +0800

    360收录

---
+++ themes/next/source/js/src/360.js
@@ -0,0 +1,4 @@
+(function () {
+  var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?a812cfe2f2b648498cc73e933f28aa9f" : "https://jspassport.ssl.qhimg.com/11.0.1.js?a812cfe2f2b648498cc73e933f28aa9f";
+  document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
