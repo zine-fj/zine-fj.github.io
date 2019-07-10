2bf41e3ded109190b56bb2c18859ebc8c1a8a938
zine-fj <747810974@qq.com>
2019-07-10 15:08:30 +0800

    博客首页

---
+++ js/app.js
@@ -0,0 +1,120 @@
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 20,
+        "density": {
+          "enable": true,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#6289ad"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 5,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 8,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 30,
+          "size_min": 2,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 500,
+        "color": "#6289ad",
+        "opacity": 0.2,
+        "width": 1.5
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 800,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 800,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 600,
+          "size": 30,
+          "duration": 2,
+          "opacity": 3,
+          "speed": 6
+        },
+        "repulse": {
+          "distance": 100
+        },
+        "push": {
+          "particles_nb": 1
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
